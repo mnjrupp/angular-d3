a9c4ade0ca7c3ca72c08f70ab09403231cc080f7
mnjrupp <mnjrupp@hotmail.com>
2021-12-30 15:22:32 -0600

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import {BarComponent} from './bar/bar.component';
 import {ScatterComponent} from './scatter/scatter.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {FileUploadComponent} from './file-upload/file-upload.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, HelloComponent,PieComponent,BarComponent,ScatterComponent ],
+  declarations: [ AppComponent, HelloComponent,PieComponent,BarComponent,ScatterComponent,FileUploadComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
