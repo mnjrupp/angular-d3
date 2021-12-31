b5442740fb49d8c5c6972fbe59c4e3756bb57095
mnjrupp <mnjrupp@hotmail.com>
2021-12-31 12:27:19 -0600

    2 files added

---
+++ src/app/app.global.ts
@@ -0,0 +1,15 @@
+
+import csvFile from './assets/frameworks.csv';
+
+export class AppGlobals {
+
+  public static jsonData =  [
+    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
+    {"Framework": "React", "Stars": "150793", "Released": "2013"},
+    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
+    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
+    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
+  ];
+
+  public static csvDataSet = csvFile;
+}
